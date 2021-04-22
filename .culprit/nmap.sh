08c947bea368d6b20448d0d211b6bb501ee12709
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-04-22 11:57:35 +0530

    REMOVED A MAJOR BUG

--- .culprit/nmap.sh
+++ .culprit/nmap.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sudo bash -c 'echo $(sudo nmap -sn 192.168.1.7/24 | grep MAC | cut -c14-21) >> new.txt && python3 nmap.py && rm new.txt'
+ip=$(hostname -I)
+nip=$(echo "$ip" | xargs)
+cla=$(ip a | grep $nip)
+read string1 string2 string3 <<< "$cla"
+folder=/usr/share/nmap
+if [ -d $folder ]
+then
+sudo bash -c "echo '$(sudo nmap -sn $string2 | grep MAC | cut -c14-21)' >> new.txt && python3 nmap.py && rm new.txt"
+else
+sudo apt install nmap
+sudo bash -c "echo '$(sudo nmap -sn $string2 | grep MAC | cut -c14-21)' >> new.txt && python3 nmap.py && rm new.txt"
+fi
+
--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-chmod +x .culprit/main.sh .culprit/nmap.py .culprit/nmap.sh culprit.desktop && sudo cp -r .culprit /usr/ && sudo cp culprit.desktop /usr/share/applications
+chmod +x .culprit/main.sh .culprit/nmap.py .culprit/nmap.sh culprit.desktop && sudo cp -r .culprit /usr/ && sudo cp culprit.desktop /usr/share/applications && sudo cp culprit.desktop ~/Desktop 
